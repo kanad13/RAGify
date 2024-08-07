79b2043e7303af99540f3ef98360f3bd5893419c
Kunal Pathak <kunalpathak13@gmail.com>
2024-08-07 12:33:10 +0200

    welcome page corrected

--- Welcome.py
+++ Welcome.py
@@ -1,20 +1,22 @@
 import streamlit as st
 
 config = {'scrollZoom': True, 'displayModeBar': True, 'displaylogo': False}
-st.set_page_config(page_title="RAGify", page_icon=":soccer:", layout="wide", initial_sidebar_state="expanded", menu_items=None)
+st.set_page_config(page_title="RAGify", page_icon=":chat-plus-outline:", layout="wide", initial_sidebar_state="expanded", menu_items=None)
 
 st.write("""
 #### RAGify - Bringing Your Documentation to Life with Generative AI
 
-This webapp is for RAGify - a tool that shows you how to transform your internal company documents into an interactive Q&A system. It uses two key technologies:
+This chatbot is for RAGify - a tool that shows you how to transform your internal company documents into an interactive Q&A system.\n
+
+It uses two key technologies:
 
 1. Retrieval-Augmented Generation (RAG)
 2. Large Language Models (LLMs)
 
 #### Why RAGify?
 
-Every company has a vast amount of documentation spread across various file formats like PDF and DOCX.
-Wouldn't it be great if you could chat with these documents?
+Every company has a vast amount of documentation spread across various file formats like PDF and DOCX.\n
+Would it not be great if you could chat with these documents?
 
 If you use ChatGPT or Gemini to upload your documents to chat with them, then that risks making your company's confidential information public.
 
@@ -31,5 +33,3 @@ Click on the sidebar to interact with RAGify App. \n
 If you would like to look at the code of the app, then [jump to Github](https://github.com/kanad13/RAGify). \n
 If you would like to know more about me, **Kunal Pathak**, the creator of this app, then [checkout my website](https://www.kunal-pathak.com).
 				 """)
-
-#st.image('late/projects/UEFA-Euro-Data-Analysis/assets/soccer_player.jpeg', caption=None, width=400, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
